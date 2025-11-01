bece407c211c310cebaf216e8feb8daaea0ad1f0
Lawrence9092 <lngwenya@byupathway.edu>
2025-11-01 16:56:32 +0200

    edits

--- scripts/date.js
+++ scripts/date.js
@@ -1,5 +1,3 @@
-// Copyright Year
 document.getElementById("year").textContent = new Date().getFullYear();
-
-// Last Modified
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
