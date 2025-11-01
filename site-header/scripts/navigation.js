5e0574962bd97111d5c18f71c67304bbab3c7e55
Lawrence9092 <lngwenya@byupathway.edu>
2025-11-01 16:02:53 +0200

    Removed from site header

--- site-header/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menu');
-const navMenu = document.getElementById('navMenu');
-
-menuButton.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
