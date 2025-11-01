006a782dbeca46d8481a30d991179924389d1a49
Lawrence9092 <lngwenya@byupathway.edu>
2025-11-01 15:56:25 +0200

    Homepage

--- site-header/scripts/navigation.js
+++ site-header/scripts/navigation.js
@@ -1,7 +1,6 @@
-// Store the selected elements that we are going to use.
-const navbutton = document.querySelector("ham-btn");
+const menuButton = document.getElementById('menu');
+const navMenu = document.getElementById('navMenu');
 
-// Toggle the show class on and off
-navbutton.addEventListListerner("click", () => {
-    navbutton.classList.toggle("show");
+menuButton.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
 });
