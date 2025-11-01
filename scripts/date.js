78093efa16b09ceb68642a641af85ad13750e3f1
Lawrence9092 <lngwenya@byupathway.edu>
2025-11-01 16:30:28 +0200

    folder location

--- scripts/date.js
+++
@@ -1,5 +0,0 @@
-// Copyright Year
-document.getElementById("year").textContent = new Date().getFullYear();
-
-// Last Modified
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
